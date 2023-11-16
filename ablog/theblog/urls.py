2ae635c4204e802a231918890da39797e5f278df
netoxdw <neto.guevara.tapia@gmail.com>
2023-11-16 15:47:47 -0800

    Video 2

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from theblog import views
 
+# **from theblog import views
+
+from .views import IndexView, ArticleDetailView
 urlpatterns = [
-    path('', views.index, name="index"), #Donde index es una funcion a mostrar como template
+    #Donde index es una funcion a mostrar como template
+    # **path('', views.index, name="index"), #Donde index es una funcion a mostrar como template
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+# Utilizando las vistas genericas de django
+# Listview Esta consulta generica trae los posts y los enlista
+# DetailView Al dar click nos muestra el post 
+from django.views.generic import ListView, DetailView
+from .models import Post
+
 # Create your views here.
-def index(request):
-	""" Vista para atender la petición de la url / """
-	return render(request, "theblog/index.html", {})
+
+# La funcion index es exportada a urls.py de app
+# def index(request):
+# 	""" Vista para atender la petición de la url / """
+# 	return render(request, "theblog/index.html", {})
+
+# La funcion indexView es exportada a urls.py de app
+class IndexView(ListView):
+    model = Post
+    template_name = 'index.html' 
+    
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'

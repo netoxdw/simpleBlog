080135ba0f5894c65ddfebd2974913103857a341
netoxdw <neto.guevara.tapia@gmail.com>
2023-11-25 12:40:40 -0800

    video 6

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Utilizando las vistas genericas de django
 # Listview Esta consulta generica trae los posts y los enlista
 # DetailView Al dar click nos muestra el post 
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 # Create your views here.
 
 
@@ -23,3 +23,9 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ('title', 'title_tag', 'body')
